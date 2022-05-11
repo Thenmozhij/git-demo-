ee594929eb54f090dc68549402d7648b6502d0e5
thenmozhi <thenmozhigce@gmail.com>
2022-05-11 14:59:01 +0530

    added project to git local repo

---
+++ src/main/java/com/example/SpringGitProject/SpringGitProjectApplication.java
@@ -0,0 +1,34 @@
+package com.example.SpringGitProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import models.Book;
+
+@SpringBootApplication
+@RestController
+public class SpringGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitProjectApplication.class, args);
+	}
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		
+		return "Welcome to the page";
+	}
+	
+	@GetMapping("/check")
+	public ResponseEntity<Book> check() {
+		
+		return new ResponseEntity<>(new Book(1L,"Book1"),HttpStatus.OK);
+	}
+	
+	
+
+}
---
+++ src/main/java/models/Book.java
@@ -0,0 +1,18 @@
+package models;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Component
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+	Long bookId;
+	String bookName;
+
+}
